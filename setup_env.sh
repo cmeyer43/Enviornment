cc78e654e63277e4ed3fa9fcf9596ad549cd4424
Corey <mail2coreym@gmail.com>
2024-08-16 16:32:23 -0400

    updated

--- setup_env.sh
+++ setup_env.sh
@@ -1,16 +1,34 @@
-sudo apt-get install -y tmux vim xclip build-essential gitk git docker.io
-cp ~/.bashrc ~/.bashrc.old
-cat bashrc > ~/.bashrc
 cp ~/.vimrc ~/.vimrc.old
-cat vimrc > ~/.vimrc
-sudo touch /etc/vim/vimrc.local
-sudo cat vimrc > /etc/vim/vimrc.local
-cp ~/.tmux.conf ~/.tmux.conv.old
-cat tmux.conf > ~/.tmux.conf
-source ~/.bashrc
-tmux source ~/.tmux.conf
-sudo groupadd docker
-sudo usermod -aG docker $USER
-newgrp
-newgrp docker
+cp ~/.tmux.conf ~/.tmux.conf.old
+name="$(uname -s)"
+case "${name}" in
+    Linux*)
+        sudo apt-get install -y tmux vim xclip build-essential gitk git docker.io
+        cp ~/.bashrc ~/.bashrc.old
+        cat bashrc > ~/.bashrc
+        cat vimrc > ~/.vimrc
+        sudo touch /etc/vim/vimrc.local
+        sudo cat vimrc > /etc/vim/vimrc.local
+        cat tmux.conf > ~/.tmux.conf
+        source ~/.bashrc
+        cat bashrc > ~/.bashrc
+        sudo touch /etc/vim/vimrc.local
+        sudo cat vimrc > /etc/vim/vimrc.local
+        cat tmux.conf > ~/.tmux.conf
+        source ~/.bashrc
+        sudo groupadd docker
+        sudo usermod -aG docker $USER
+        newgrp
+        newgrp docker
 chmod 777 /var/run/docker.sock
+        echo "Linux";;
+    Darwin*)
+        brew install tmux vim xclip git gitx docker -q
+        cp ~/.zshrc ~/.zshrc.old
+        cat zshrc > ~/.zshrc
+        cat tmuxApple.conf >~/.tmux.conf
+        source ~/.zshrc
+        echo "mac";;
+esac
+cat vimrc > ~/.vimrc
+tmux source ~/.tmux.conf

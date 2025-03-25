aff487246d282343ef9acb7f77ca6f1e16156f17
Corey Meyer <corey.meyer@gtri.gatech.edu>
2025-03-25 16:19:50 -0400

    docker will not mount on root

--- docker/start_docker.sh
+++ docker/start_docker.sh
@@ -15,14 +15,26 @@ tag=$1
 
 set -x
 
-exec docker run --rm -it \
-    --privileged \
-    --net host \
-    -v $HOME:$HOME \
-    --security-opt seccomp=unconfined \
-    --hostname=$(hostname) --env DISPLAY=$DISPLAY \
-    -v /tmp/.X11-unix:/tmp/.X11-unix \
-    --name $container \
-    $tag /_setup_user_shell.sh start \
-        "$PWD" "$USER" "$HOME" $USERID $GROUPID
+if [ "${EUID:-$(id -u)}" -ne 0 ]; then
+    exec docker run --rm -it \
+        --privileged \
+        --net host \
+        -v $HOME:$HOME \
+        --security-opt seccomp=unconfined \
+        --hostname=$(hostname) --env DISPLAY=$DISPLAY \
+        -v /tmp/.X11-unix:/tmp/.X11-unix \
+        --name $container \
+        $tag /_setup_user_shell.sh start \
+            "$PWD" "$USER" "$HOME" $USERID $GROUPID
 
+else # do not mount home onto root that is bad.
+    exec docker run --rm -it \
+        --privileged \
+        --net host \
+        --security-opt seccomp=unconfined \
+        --hostname=$(hostname) --env DISPLAY=$DISPLAY \
+        -v /tmp/.X11-unix:/tmp/.X11-unix \
+        --name $container \
+        $tag /_setup_user_shell.sh start \
+            "$PWD" "$USER" "$HOME" $USERID $GROUPID
+fi
